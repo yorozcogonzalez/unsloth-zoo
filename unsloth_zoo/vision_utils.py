df49b72dcd84f9275a83d8526c616a94772d38c2
Yoelvis Orozco Gonzalez <yoelvis.orozco@gmail.com>
2025-06-11 18:47:42 -0400

    test

--- unsloth_zoo/vision_utils.py
+++ unsloth_zoo/vision_utils.py
@@ -270,7 +270,7 @@ class UnslothVisionDataCollator:
         processor,
         max_seq_length  = None,
         formatting_func = None,
-        resize = "max", # Can be (10, 10) or "min" to resize to fit
+        resize = "min", # Can be (10, 10) or "min" to resize to fit
                         # the model's default image_size or "max"
                         # for no resizing and leave image intact
         ignore_index = -100,
